405e905e2af103f0e1619ec89a2c7ed9730962be
Retsomm <112182ssss@gmail.com>
2025-08-25 22:19:08 +0800

    fix: remove unused Accordion import from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import UnityParticleSystem from "../component/UnityParticleSystem";
-import Accordion from "../component/Accordion";
 
 const sections = [
   {
